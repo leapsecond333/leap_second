4e8ecfcb0e895e378500c2d82c6ecdef4c6b1998
leapsecond333 <120238483+leapsecond333@users.noreply.github.com>
2023-01-14 17:56:30 +0900

    Add files via upload

--- 조성환.py
+++ 조성환.py
@@ -1,43 +1,40 @@
 import requests
 
-
 headers = \
         {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
 
-date = [20230103, 20230104, 20230105, 20230106, 20230107]
-page = [1, 2, 3, 4]
-
-for i in range(0, 5):
-    for j in range(page[3]):
-        url = 'https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid2=731&sid1=105&date={0}&page={1}'.format(date[i], page[j])           
-
-        if j == page[1]:
-            extract_data1_save_data = source_data1[:pos2]
-            if extract_data1_save_data == extract_data1:
-                break
-        
-        site = requests.get(url, headers=headers)                                                       
-        source_data1 = site.text
-        source_data2 = site.text
-
-        count1 = source_data1.count('72" alt="')
-        count2 = source_data2.count('class="lede">')
-
-        print(date[i], page[j],'='*60)
-        for k in range(count1):                                                                         
-            pos1 = source_data1.find('72" alt="')+ len('72" alt="')                                       
-            source_data1 = source_data1[pos1:]
+url = 'https://www.todayhumor.co.kr/board/list.php?table=bestofbest'          
     
-            pos2 = source_data1.find('onError="javascript:this.src=')
-            extract_data1 = source_data1[: pos2]
+site = requests.get(url, headers=headers)                                                       
+source_data1 = site.text
+source_data2 = site.text
 
-            source_data1 = source_data1[pos2+1:]
+count1 = source_data1.count('&page=1" target="_top">')
 
-            pos3 = source_data2.find('class="lede">')+ len('class="lede">')
-            source_data2 = source_data2[pos3:]
+for k in range(count1):                                                             # 링크2 subject"><a href="    " target="_top">
+    pos1 = source_data1.find('&page=1" target="_top">')+ len('&page=1" target="_top">')
+    source_data1 = source_data1[pos1:]
 
-            pos4 = source_data2.find('…</span>')
-            extract_data2 = source_data2[:pos4]
+    pos2 = source_data1.find("</a><span class='")
+    extract_data1 = source_data1[: pos2]
+    source_data1 = source_data1[pos2+1:]
 
-            source_data2 = source_data2[pos4+1:]
-            print('     ', extract_data1, '\n    : ', extract_data2)
+
+    pos3 = source_data2.find(""""><img src='""")+ len(""""><img src='""")
+    source_data2 = source_data2[pos3:]
+    
+    pos4 = source_data2.find("bgm_icon'/")
+    extract_data2 = source_data2[: pos4]
+    source_data2 = source_data2[pos4+1:]
+
+
+    pos3 = source_data2.find('subject"><a href="')+ len('subject"><a href="')
+    source_data2 = source_data2[pos3:]
+    
+    pos4 = source_data2.find('" target="_top">')
+    extract_data2 = source_data2[: pos4]
+    source_data2 = source_data2[pos4+6:]
+
+
+    print('     ', extract_data1, '\n')
+    print('     ', extract_data2, '\n')
