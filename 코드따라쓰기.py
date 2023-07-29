3b3497b55a291c7077993cf2474dfec9d86bc7f6
leapsecond333 <120238483+leapsecond333@users.noreply.github.com>
2023-07-29 17:46:11 +0900

    Add files via upload

--- 코드따라쓰기.py
+++ 코드따라쓰기.py
@@ -3,8 +3,19 @@ from pygame.color import Color
 from pygame.sprite import Sprite
 from pygame.surface import Surface
 from runner import Runner
+import time
 
 FPS = 28
+
+key_status = ""
+key = None
+m = 70
+v = 0
+F = 0
+
+
+heading = None
+
 
 class Bullet(Sprite):
     def __init__(self):
@@ -16,7 +27,7 @@ class Bullet(Sprite):
         self.rect = self.image.get_rect()
 
     def update(self):
-        self.rect.x -= 1
+        self.rect.x -= 6
 
 if __name__ == "__main__":
     pygame.init()
@@ -32,26 +43,19 @@ if __name__ == "__main__":
 
     runner1 = Runner()
     runner1.rect.x = 0
-    runner1.rect.y = 170
+    runner1.rect.y = 170.0
 
-    runner2 = Runner()
-    runner2.rect.x = 130
-    runner2.rect.y = 170
-
-    runner3 = Runner()
-    runner3.rect.x = 250
-    runner3.rect.y = 170
+    
 
     runner_group = pygame.sprite.Group()
     runner_group.add(runner1)
-    runner_group.add(runner2)
-    runner_group.add(runner3)
-
+    
     bullet = Bullet()
     bullet.rect.x = screen.get_width()
     bullet.rect.y = 200
     bullet_group = pygame.sprite.Group()
     bullet_group.add(bullet)
+
 
     # 게임 루프
     while run:
@@ -59,6 +63,10 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            elif event.type == pygame.KEYUP:
+                key = None
+            elif event.type == pygame.KEYDOWN:
+                key = event.key
 
         # 2) 게임 상태 업데이트
         runner_group.update()
@@ -73,6 +81,29 @@ if __name__ == "__main__":
         runner_group.draw(screen)
         bullet_group.draw(screen)
         pygame.display.flip()
+
+
+        # 점프 (최고점에 다가갈 수록 느려지게 만드는 중)
+        if key == pygame.K_UP:
+            heading = "up"
+        if heading == "up":
+            runner1.rect.y -= F / 100
+            F = 1/2 * m * v * v
+            v -= 1
+            if runner1.rect.y <= 50:
+                heading = "down"
+                F = 0
+                v = 0
+        if heading == "down":
+            runner1.rect.y += F / 100
+            F = 1/2 * m * v * v
+            v += 1
+            if runner1.rect.y >= 170:
+                heading = None
+                F = 0
+                v = 10
+        print(F, "=", m, "*", v)
+            
 
         clock.tick(FPS)
 
